c13e0dd9ae76a734b53aed886f183d1cadea7f5f
embezzlez <ryujinapp@gmail.com>
2021-12-10 12:23:28 +0700

    constant

---
+++ src/constant.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+define('SEPARATOR',DIRECTORY_SEPARATOR);
+define('PUBLIC_PATH',__DIR__.'/');
+define('APP_PATH',dirname(__DIR__) . '/app/');
+define('CORE_PATH',dirname(__DIR__).'/core/');
+define('LANG_PATH',dirname(__DIR__) .'/core/languages/');
+define('BOT_PATH',dirname(__DIR__).'/core/bots/');
+define('FUNC_PATH',dirname(__DIR__) .'/core/functions/');
+define('LIB_PATH',dirname(__DIR__) .'/core/libraries/');
+
+define('REQ_PATH', dirname(__DIR__) .'/app/request/');
+define('PAGE_PATH',dirname(__DIR__) .'/app/pages/');
+define('CONFIG_PATH',dirname(__DIR__) .'/app/config/');
